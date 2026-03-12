7913fbd92520934baece9fe92a947cd7928e8173
hojeong <parkhj062@gmail.com>
2026-03-12 15:53:33 +0900

    ios > initial commit

---
+++ ios/iosTests/iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  iosTests.swift
+//  iosTests
+//
+//  Created by hj on 3/12/26.
+//
+
+import Testing
+@testable import ios
+
+struct iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
